369b4df484dd782ee35163ff95e80b49446c5221
Bruno Galvao <bruno_galvao@outlook.com>
2023-11-01 18:27:45 -0300

    fix: start task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import { Task } from './task/task';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'kanban-fire';
+
+  todo: Task[] = [
+    {
+      title: 'Buy milk',
+      description: 'Go to the store and buy milk',
+    },
+    {
+      title: 'Create an app',
+      description: 'Create an app using Angular',
+    },
+  ];
 }
