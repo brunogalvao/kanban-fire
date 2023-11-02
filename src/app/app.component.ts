2d494cc510b087cbeddc0bda5723c26f6c753294
Bruno Galvao <bruno_galvao@outlook.com>
2023-11-02 20:50:14 -0300

    update: add firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,20 +3,16 @@ import { Task } from './task/task';
 import { CdkDragDrop, transferArrayItem } from '@angular/cdk/drag-drop';
 import { MatDialog } from '@angular/material/dialog';
 import {
-  TaskDialogComponent,
   TaskDialogResult,
+  TaskDialogComponent,
 } from './task-dialog/task-dialog.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  constructor(private dialog: MatDialog) {}
-
-  title = 'kanban-fire';
-
   todo: Task[] = [
     {
       title: 'Buy milk',
@@ -24,28 +20,13 @@ export class AppComponent {
     },
     {
       title: 'Create a Kanban app',
-      description: 'Using Firebase and Angular, create a Kanban app',
+      description: 'Using Firebase and Angular create a Kanban app!',
     },
   ];
   inProgress: Task[] = [];
   done: Task[] = [];
 
-  editTask(list: string, task: Task): void {}
-
-  drop(event: CdkDragDrop<Task[]>): void {
-    if (event.previousContainer === event.container) {
-      return;
-    }
-    if (!event.container.data || !event.previousContainer.data) {
-      return;
-    }
-    transferArrayItem(
-      event.previousContainer.data,
-      event.container.data,
-      event.previousIndex,
-      event.currentIndex
-    );
-  }
+  constructor(private dialog: MatDialog) {}
 
   newTask(): void {
     const dialogRef = this.dialog.open(TaskDialogComponent, {
@@ -54,13 +35,45 @@ export class AppComponent {
         task: {},
       },
     });
-    dialogRef
-      .afterClosed()
-      .subscribe((result: TaskDialogResult | undefined) => {
-        if (!result) {
-          return;
-        }
-        this.todo.push(result.task);
-      });
+    dialogRef.afterClosed().subscribe((result: TaskDialogResult) => {
+      if (!result) {
+        return;
+      }
+      this.todo.push(result.task);
+    });
+  }
+
+  editTask(list: 'done' | 'todo' | 'inProgress', task: Task): void {
+    const dialogRef = this.dialog.open(TaskDialogComponent, {
+      width: '270px',
+      data: {
+        task,
+        enableDelete: true,
+      },
+    });
+    dialogRef.afterClosed().subscribe((result: TaskDialogResult) => {
+      if (!result) {
+        return;
+      }
+      const dataList = this[list];
+      const taskIndex = dataList.indexOf(task);
+      if (result.delete) {
+        dataList.splice(taskIndex, 1);
+      } else {
+        dataList[taskIndex] = task;
+      }
+    });
+  }
+
+  drop(event: CdkDragDrop<Task[]>): void {
+    if (event.previousContainer === event.container) {
+      return;
+    }
+    transferArrayItem(
+      event.previousContainer.data,
+      event.container.data,
+      event.previousIndex,
+      event.currentIndex
+    );
   }
 }
